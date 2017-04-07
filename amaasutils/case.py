2915303cf60b34a6347fe81fae1f1ef57fb87aed
Ned Lowe <ned@amaas.com>
2017-04-07 13:01:26 +0800

    Handle ‘keyed’ children - e.g. for AMaaS Models, the children are keyed dictionaries, and we don’t want to convert those keys.  Handle lists of children as well (e.g. for Links).  AMAAS-648.

--- amaasutils/case.py
+++ amaasutils/case.py
@@ -1,26 +1,64 @@
 """ Helper functions for converting string cases """
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import re
 
 
-def dict_camel_to_snake_case(camel_dict):
+def dict_camel_to_snake_case(camel_dict, convert_keys=True, convert_subkeys=False):
     """
     Recursively convert camelCased keys for a camelCased dict into snake_cased keys
+
+    :param camel_dict: Dictionary to convert
+    :paran convert_keys: Whether the key should be converted
+    :param convert_subkeys: Whether to also convert the subkeys, in case they are named properties of the dict
+    :return:
     """
+
     converted = {}
     for key, value in camel_dict.items():
-        new_value = dict_camel_to_snake_case(value) if isinstance(value, dict) else value
-        converted[to_snake_case(key)] = new_value
+        if isinstance(value, dict):
+            new_value = dict_camel_to_snake_case(value, convert_keys=convert_subkeys,
+                                                 convert_subkeys=True)
+        elif isinstance(value, list):
+            new_value = []
+            for subvalue in value:
+                new_subvalue = dict_camel_to_snake_case(subvalue, convert_keys=convert_subkeys,
+                                                        convert_subkeys=True) \
+                    if isinstance(subvalue, dict) else subvalue
+                new_value.append(new_subvalue)
+        else:
+            new_value = value
+        new_key = to_snake_case(key) if convert_keys else key
+        converted[new_key] = new_value
     return converted
 
 
-def dict_snake_to_camel_case(snake_dict):
+def dict_snake_to_camel_case(snake_dict, convert_keys=True, convert_subkeys=False):
     """
     Recursively convert a snake_cased dict into a camelCased dict
+
+    :param snake_dict: Dictionary to convert
+    :param convert_keys: Whether the key should be converted
+    :param convert_subkeys: Whether to also convert the subkeys, in case they are named properties of the dict
+    :return:
     """
+
     converted = {}
     for key, value in snake_dict.items():
-        new_value = dict_snake_to_camel_case(value) if isinstance(value, dict) else value
-        converted[to_camel_case(key)] = new_value
+        if isinstance(value, dict):
+            new_value = dict_snake_to_camel_case(value, convert_keys=convert_subkeys,
+                                                 convert_subkeys=True)
+        elif isinstance(value, list):
+            new_value = []
+            for subvalue in value:
+                new_subvalue = dict_snake_to_camel_case(subvalue, convert_keys=convert_subkeys,
+                                                        convert_subkeys=True) \
+                    if isinstance(subvalue, dict) else subvalue
+                new_value.append(new_subvalue)
+        else:
+            new_value = value
+        new_key = to_camel_case(key) if convert_keys else key
+        converted[new_key] = new_value
     return converted
 
 
