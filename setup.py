f2ff804f51eef50c77e9f76e2e15daa922deaec2
paul-rs <roland.paul.santos@gmail.com>
2017-11-17 18:21:50 +0800

    AMAAS-1186 Updating hash function for nested lists and dicts

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = [
 
 setup(
     name='amaasutils',
-    version='1.2.9',
+    version='1.3.4',
     description='Asset Management as a Service - Utils',
     license='Apache License 2.0',
     url='https://github.com/amaas-fintech/amaas-utils-python',
